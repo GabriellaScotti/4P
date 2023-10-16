994f4e98263080d4d800f994a7a73957b157bee3
GabriellaScotti <148056129+GabriellaScotti@users.noreply.github.com>
2023-10-16 11:58:03 +0200

    Add files via upload

---
+++ 03- Tabella/src/Tabella.java
@@ -0,0 +1,17 @@
+
+public class Tabella {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		System.out.println("Persona Altezza (cm)\tMisura Scarpe");
+		System.out.println("=====================================");
+		System.out.print("Luisa\t160 \t \t36 \nMarco\t180 \t \t43 \nGiulia\t168 \t \t38");
+		*/
+		
+		//crea il codice in una sola linea concatenata
+		System.out.println("Persona Altezza (cm) \tMisura Scarpe" + "\n" + "===================================== \n" + "Luisa\t160 \t \t36 \nMarco\t180 \t \t43 \nGiulia\t168 \t \t38");
+		
+	}
+
+}
