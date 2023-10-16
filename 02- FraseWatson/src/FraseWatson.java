6aaff7cf7a86797566c6a15ad2a5672451ca464c
GabriellaScotti <148056129+GabriellaScotti@users.noreply.github.com>
2023-10-16 11:57:20 +0200

    Add files via upload

---
+++ 02- FraseWatson/src/FraseWatson.java
@@ -0,0 +1,10 @@
+
+public class FraseWatson {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Credo che in tutto il mondo ci sia mercato per forse cinque computer.");
+		System.out.print("Thomas Watson, IBM, 1943");
+	}
+
+}
